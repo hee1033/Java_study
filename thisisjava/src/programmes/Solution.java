9915e86da7d09161141f876b872f5c33b5ab1e60
hee1033 <lgp920024@gmail.com>
2022-09-02 10:33:59 +0900

    0901

---
+++ thisisjava/src/programmes/Solution.java
@@ -0,0 +1,24 @@
+package programmes;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = 987;
+		int result = 0;
+		
+		String str = String.valueOf(n);
+		String[] sum = str.split("");
+		
+		for(int i=0;i<sum.length;i++) {
+			result += Integer.parseInt(sum[i]);
+		}
+		
+		System.out.println(result);
+	}
+
+}
