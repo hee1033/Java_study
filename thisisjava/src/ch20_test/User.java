ef162e05f4416f6d52f74391c6f8b9d3e3af4c8d
hee1033 <lgp920024@gmail.com>
2022-10-19 08:24:41 +0900

    10.18

---
+++ thisisjava/src/ch20_test/User.java
@@ -0,0 +1,12 @@
+package ch20_test;
+
+import lombok.Data;
+
+@Data
+public class User {
+	private String userid;
+	private String userName;
+	private String userPassword;
+	private int userAge;
+	private String userEmail;
+}
